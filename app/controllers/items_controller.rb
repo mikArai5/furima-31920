be1c49d5a5d63a91f0aee181f974fc4522db215e
mikArai5 <74860483+mikArai5@users.noreply.github.com>
2021-02-06 16:20:59 +0900

    before_action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 before_action :authenticate_user!, except: [:index, :show]
-
+before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -20,19 +20,15 @@ before_action :authenticate_user!, except: [:index, :show]
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless @item.user_id == current_user.id
       redirect_to action: :index
     end
   end
   
   def update
-    @item = Item.find(params[:id])
-
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -45,4 +41,9 @@ before_action :authenticate_user!, except: [:index, :show]
   def item_params
     params.require(:item).permit(:item_name, :explain, :category_id, :condition_id, :fee_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
