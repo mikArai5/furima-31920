65933896cb64abf2aab97d450d0a4335d6785681
mikArai5 <74860483+mikArai5@users.noreply.github.com>
2021-02-06 11:37:15 +0900

    has_one_attached　削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,6 @@ class Item < ApplicationRecord
   belongs_to :day
   belongs_to :user
   has_one_attached :image
-  has_one_attached :order
 
   validates :price, numericality: {with: /\A[0-9]+\z/, message: 'Half-width number'}
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
