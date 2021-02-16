51389a1c095509ee08f4c5d978a45b942bdf0f40
mikArai5 <74860483+mikArai5@users.noreply.github.com>
2021-02-16 22:03:01 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :day
   belongs_to :user
   has_one_attached :image
-  #has_one :order
+  has_one :order
 
   validates :price, numericality: {with: /\A[0-9]+\z/, message: 'Half-width number'}
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
