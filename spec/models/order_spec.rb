a4e87616e1b78136601110a81d93c29cb16e827a
mikArai5 <74860483+mikArai5@users.noreply.github.com>
2021-02-16 21:56:04 +0900

    user_order テストコード 記述

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  before do
+    @order = FactoryBot.build(:order)
+  end
+
+  it "tokenが空では登録できないこと" do
+    @order.token = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Token can't be blank")
+  end
+end
