ee1acdd251ec7b36095fb28be6d862b5fdbb787a
mikArai5 <74860483+mikArai5@users.noreply.github.com>
2021-02-17 22:23:55 +0900

    Update user_order.rb

--- spec/factories/user_order.rb
+++ spec/factories/user_order.rb
@@ -7,6 +7,5 @@ FactoryBot.define do
     house_number            { '青山1-1-1' }
     building_name           { '柳ビル103' }
     phone_number            { '09012345678' }
-    association :user
   end
 end
